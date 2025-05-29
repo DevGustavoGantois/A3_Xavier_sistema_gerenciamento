cb8f8d48c0d6a20dff670b1daf05132051462a38
Gustavo Gantois <gugagantois@gmail.com>
2025-05-29 16:48:13 -0300

    feat:Create register page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,6 @@ export default function Page() {
             <LoginForm />
           </CardContent>
         </Card>
-        
     </div>  
   );
 }
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,7 +1,17 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+
 export default function Register() {
     return (
-        <section className="">
-            
+        <section className="mt-40 lg:mt-60 max-w-[900px] mx-auto p-8 lg:p-0">
+            <Card>
+                <CardHeader>
+                    <CardTitle className="text-2xl">Cadastre-se</CardTitle>
+                    <CardDescription>Cadastre-se para ter acesso a nossa plataforma.</CardDescription>
+                    <CardContent>
+                        
+                    </CardContent>
+                </CardHeader>
+            </Card>
         </section>
     )
 }

d7d3797bb0bf87c86921a7d8505152efea546961
Gustavo Gantois <gugagantois@gmail.com>
2025-05-29 17:12:51 -0300

    feat:Create button of navigation register page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { LoginForm } from "@/components/c-login-form";
+import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import Link from "next/link";
 
 export default function Page() {
   return (
-    <div className="mt-20 lg:mt-60 p-8 lg:p-0 max-w-[800px] mx-auto">
+    <div className="mt-20 lg:mt-60 p-8 lg:p-0 max-w-[800px] mx-auto flex flex-col justify-between">
         <Card>
           <CardHeader>
             <CardTitle className="text-2xl">Log in</CardTitle>
@@ -13,6 +15,13 @@ export default function Page() {
             <LoginForm />
           </CardContent>
         </Card>
+        <div className="pt-80 flex items-center justify-center">
+          <Button variant="ghost" asChild>
+            <Link href="/register">
+            Cadastrar-se
+            </Link>
+          </Button>
+        </div>
     </div>  
   );
 }

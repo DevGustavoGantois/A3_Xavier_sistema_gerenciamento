4dbf34d41ba3aaeac62f5647ca223f1d9e464961
Gustavo Gantois <gugagantois@gmail.com>
2025-05-30 17:42:52 -0300

    feat:Create mockup data.

--- src/app/dashboard/reports/page.tsx
+++ src/app/dashboard/reports/page.tsx
@@ -1,6 +1,12 @@
+import OnlyFor from "@/components/OnlyFor";
+
 export default function ManagerDashboard() {
     return (
-        <>
-        </>
+        <OnlyFor roleAllowed={["gerente"]}>
+      <div>
+        <h1>Relatórios</h1>
+        <p>Conteúdo visual de relatórios.</p>
+      </div>
+    </OnlyFor>
     )
 }
